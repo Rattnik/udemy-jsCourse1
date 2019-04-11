fc5fc741e6012f6c44879bfe2dfc01046e326cc3
Alex Rattnik <lohma4@yandex.ru>
2019-04-11 11:16:38 +0300

    1 Task

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
-var varOne = 1;
-let varTwo =2;
-console.log(varOne);
+let money, time;
+
+money = prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
+
+var appData = {
+    budget : money,
+    timeData: time,
+    expnses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+var answer1, answer2;
+answer1 = prompt("Введите обязательную статью расходов в этом месяце");
+answer2 = prompt("Во сколько обойдется?");
+appData.expnses.answer1=answer2;
+answer1 = prompt("Введите обязательную статью расходов в этом месяце");
+answer2 = prompt("Во сколько обойдется?");
+appData.expnses.answer1=answer2;
+
+console.log("Ваш бюджет на 1 день=" + appData.budget/30);
